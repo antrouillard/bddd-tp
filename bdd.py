7c6c95008ba685fc75f161a65f0d6bd31c1232f4
antrouillard <antoine.rouillard@outlook.fr>
2024-12-05 17:22:32 +0100

    j'avance

--- bdd.py
+++ bdd.py
@@ -1,19 +1,13 @@
 from pymongo import MongoClient
-import time
 client = MongoClient("mongodb://localhost:27017/")
 #client = MongoClient("mongodb://localhost:27020/")
-db = client["pricesdb"]
-collection = db["prices"]
+db = client["assodb"]
+user_collection = db["users"]
 
-def searchCountry(country):
-    start = time.time()
-    resultats = collection.find({"COUNTRY":country})
-    end = time.time()
+def searchUser(mail,hashed_password):
+    resultats = user_collection.find({"MAIL":mail})
 
-    for doc in resultats[0:5]:
-        print(doc)
-    
-    print(f"Search done in {end-start:.30f}sec")
-
+    for user in resultats[0:5]:
+        print(user)
 
 client.close()
