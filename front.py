b67da1591552b233a3ca7431c264a8fb8c695aa4
Noe <noe.delvaux@free.fr>
2024-12-05 17:13:11 +0100

    Refonte Home et pages création

--- front.py
+++ front.py
@@ -1,26 +1,33 @@
 # coding: utf-8
 
 # Import Flask requirements
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, url_for
 
-from .python.secret_key import generate_secret_key
 
 # Create our Flask application object, kind of "global" variable
-app = Flask(__name__, template_folder="../templates", static_folder="../static")
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route('/bonjour')
 def bonjour():
     return "<h1>Bonjour</h1>"
 
-app.secret_key = generate_secret_key()
-
-from .python.auth.data import logged_in
 
 @app.route("/")
 def index():
-    if not logged_in():
-        return redirect("/login")
-    return render_template("home.html")
+    return render_template("/home.html")
+
+@app.route("/register")
+def register():
+    return render_template("/createUser.html")
+
+@app.route("/createGroup")
+def createGroup():
+    return render_template("/cGroup.html")
+
+@app.route("/addMatos")
+def addMatos():
+    return render_template("/addMatos.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
